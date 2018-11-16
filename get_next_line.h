68c64f15bbd19fd08005e58f5d6f6ccba31f4616
Gabriel ERHARD <gaerhard@student.42.fr>
2018-11-16 18:25:58 +0100

    added functional gnl with mutli fd supported

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gaerhard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/12 11:11:38 by gaerhard          #+#    #+#             */
+/*   Updated: 2018/11/16 11:39:53 by gaerhard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 20
+# define MAX_FD 1000
+# include "libft/libft.h"
+
+int				get_next_line(const int fd, char **line);
+
+#endif
