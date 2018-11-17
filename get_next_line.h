16c13d35f84035ee4d3a20534d16a35881813304
Gabriel ERHARD <gaerhard@student.42.fr>
2018-11-17 13:34:23 +0100

    added -I flag and compil rule to the makefile, moved libft.h to includes/

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: gaerhard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 11:11:38 by gaerhard          #+#    #+#             */
-/*   Updated: 2018/11/16 19:06:48 by gaerhard         ###   ########.fr       */
+/*   Updated: 2018/11/17 13:33:04 by gaerhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 20
-# include "libft/libft.h"
+# include "libft.h"
 
 int				get_next_line(const int fd, char **line);
 
